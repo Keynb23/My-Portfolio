fbc3c596f8c274f4601591e0a4e58c1a466ebb56
Key'n Brosdahl <106933331+Keynb23@users.noreply.github.com>
2025-05-29 22:17:28 -0500

    animation working. still needs work

--- KeynB/src/components/Skills.jsx
+++ KeynB/src/components/Skills.jsx
@@ -12,50 +12,23 @@ import CodeT from '../assets/CodeT.png';
 
 const Skills = () => {
   return (
-    <section className="skills-container">
-      <h2 className="skills-title">Skills & Technologies</h2>
-      <div className="skills-grid">
 
-        <div className="skills-card">
-          <h3 className="skills-heading">Languages</h3>
-          <div className="skills-icons animate-group">
-            <img src={JS} alt="JavaScript" className="icon animate-in delay-1" />
-            <img src={Python} alt="Python" className="icon animate-in delay-2" />
-            <img src={Html5} alt="HTML5" className="icon animate-in delay-3" />
-            <img src={Css} alt="CSS" className="icon animate-in delay-4" />
+    <div className='Skills-section-bg'>
+     <div className="banner">
+        <div className="slider" 
+>            <div className="item"><img src={JS} alt="JavaScript" /></div>
+            <div className="item"><img src={Python} alt="Python" /></div>
+            <div className="item"><img src={Html5} alt="HTML5" /></div>
+            <div className="item"><img src={Css} alt="CSS" /></div>
+            <div className="item"><img src={ReactLogo} alt="React" /></div>
+            <div className="item"><img src={TS} alt="TypeScript" /></div>
+            <div className="item"><img src={Threejs} alt="Three.js" /></div>
+            <div className="item"><img src={SQL} alt="SQL" className="icon-8" /></div>
+            <div className="item"><img src={Postman} alt="Postman" className="icon-9" /></div>
+            <div className="item"><img src={CodeT} alt="CodeT" className="icon-10" /></div>
           </div>
         </div>
-
-        <div className="skills-card">
-          <h3 className="skills-heading">Frameworks/Libraries</h3>
-          <div className="skills-icons animate-group">
-            <img src={ReactLogo} alt="React" className="icon animate-in delay-1" />
-            <img src={TS} alt="TypeScript" className="icon animate-in delay-2" />
-            <img src={Threejs} alt="Three.js" className="icon animate-in delay-3" />
-          </div>
-        </div>
-
-        <div className="skills-card">
-          <h3 className="skills-heading">Databases</h3>
-          <img src={SQL} alt="SQL" className="icon animate-in delay-2" />
-        </div>
-
-        <div className="skills-card">
-          <h3 className="skills-heading">Tools/DevOps</h3>
-          <img src={Postman} alt="Postman" className="icon animate-in delay-3" />
-        </div>
-
-        <div className="skills-card">
-          <h3 className="skills-heading">Certifications</h3>
-          <img src={CodeT} alt="CodeT" className="icon-single animate-in delay-1" />
-          <ul className="skills-cert">
-            <li>Coding Temple Software Engineering Certificate (Expected May 2025)</li>
-          </ul>
-        </div>
-
       </div>
-    </section>
-  );
-};
+)};
 
 export default Skills;
